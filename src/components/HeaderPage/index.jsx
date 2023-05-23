4391bc02c9f1a8cf877b9008ef813ebdf51c7b87
qualityseg <128048171+qualityseg@users.noreply.github.com>
2023-05-23 16:42:28 -0300

    suus

--- src/components/HeaderPage/index.jsx
+++ src/components/HeaderPage/index.jsx
@@ -1,81 +1,42 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
-import 'react-responsive-carousel/lib/styles/carousel.min.css'; // Importe os estilos do carousel
-import styles from './styles.module.scss';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
-export const HeaderPage = ({ subtitle, title }) => {
-  const [currentSlide, setCurrentSlide] = useState(0);
-  const [carouselHeight, setCarouselHeight] = useState(533);
-  const carouselRef = useRef(null);
-
-  useEffect(() => {
-    const handleResize = () => {
-      const windowHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-      const newHeight = windowHeight >= 768 ? 533 : windowHeight * 0.6;
-
-      setCarouselHeight(newHeight);
-      setCurrentSlide(0);
-    };
-
-    const handleLoad = () => {
-      const windowHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-      const newHeight = windowHeight >= 768 ? 533 : windowHeight * 0.6;
-
-      setCarouselHeight(newHeight);
-      setCurrentSlide(0);
-    };
-
-    window.addEventListener('resize', handleResize);
-    window.addEventListener('load', handleLoad);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-      window.removeEventListener('load', handleLoad);
-    };
-  }, []);
-
+export const HeaderPage = () => {
   return (
-    <section className={styles.headerPage}>
-      <Carousel
-        ref={carouselRef}
-        className={styles.carousel}
-        autoPlay={true}
-        infiniteLoop={true}
-        interval={5000}
-        afterChange={(slideIndex) => setCurrentSlide(slideIndex)}
-        selectedItem={currentSlide}
-        style={{ maxHeight: carouselHeight, overflow: 'hidden' }}
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <div
+        style={{
+          width: 500,
+          marginTop: 145,
+          marginRight: 5,
+          backgroundColor: 'gray',
+          borderRadius: 5,
+        }}
       >
-        <div className={`${styles.carouselImageWrapper} ${styles.largeScreenOnly}`}>
-          <img
-            srcSet="images/background-1.jpg"
-            sizes={`${carouselHeight}px`}
-            className={styles.carouselImage}
-            alt="Carousel Image 1"
-          />
-        </div>
-
-        <div className={styles.carouselImageWrapper}>
-          <img
-            srcSet="images/background-2.jpg"
-            sizes={`${carouselHeight}px`}
-            className={styles.carouselImage}
-            alt="Carousel Image 2"
-          />
-        </div>
-
-        <div className={styles.carouselImageWrapper}>
-          <img
-            srcSet="images/background-3.jpg"
-            sizes={`${carouselHeight}px`}
-            className={styles.carouselImage}
-            alt="Carousel Image 3"
-          />
-        </div>
-      </Carousel>
-      <div className={styles.barraVerde}>
-        <p>CONHEÇA NOSSO CATÁLOGO DE CURSOS EAD</p>
+        <Carousel autoPlay={true} infiniteLoop={true} interval={5000}>
+          <div>
+            <img src="images/background-1.jpg" alt="Carousel Image 1" />
+          </div>
+          <div>
+            <img src="images/background-2.jpg" alt="Carousel Image 2" />
+          </div>
+          <div>
+            <img src="images/background-3.jpg" alt="Carousel Image 3" />
+          </div>
+        </Carousel>
       </div>
-    </section>
+      <div
+        style={{
+          width: 500,
+          textAlign: 'center',
+          backgroundColor: 'green',
+          marginTop: 145,
+          borderRadius: 5,
+        }}
+      >
+        <p>Placeholder</p>
+      </div>
+    </div>
   );
 };
