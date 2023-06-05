f3d5daf511075269c836586432cad04b01a07487
qualityseg <128048171+qualityseg@users.noreply.github.com>
2023-06-05 14:31:28 -0300

    remove next

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/cursos_ead":["static\u002Fchunks\u002Fpages\u002Fcursos_ead.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fcursos_ead"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
